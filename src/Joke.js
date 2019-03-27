f0509477dc945820ca8efb0261b15f8829738381
Harsh <harshbhama96@gmail.com>
2019-03-27 13:02:21 +0530

    basic style

--- src/Joke.js
+++ src/Joke.js
@@ -3,11 +3,12 @@ import React from "react"
 function Joke(prop){
   console.log(prop)
 
+
     return (
 
       <div>
         <h1 style = {{display: prop.detail.Question==null ? "none" : "block"}}> Question : {prop.detail.Question} </h1>
-        <h2> PunchLine : {prop.detail.PunchLine} </h2>
+        <h2 style = {{color: !prop.detail.Question && "grey" }}> PunchLine : {prop.detail.PunchLine} </h2>
         <hr/>
       </div>
 
